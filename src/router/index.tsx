5c4c60dfa1be58bad09bd1bec3ed3835b6290f37
zhangxu <2590000714@qq.com>
2020-05-06 16:50:54 +0800

    添加404页面

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import { Home } from '../views/home/index';
 import { Detail } from '../views/detail/index';
 import { Resume } from '../views/resume/index';
 import { Comments } from '../views/comments/index';
+import { NotFound } from '../views/not-found';
 
 const routes = [
   {
@@ -24,6 +25,10 @@ const routes = [
       {
         path: '/comments',
         component: Comments
+      },
+      {
+        path: '*',
+        component: NotFound
       }
     ]
   }
---
+++ src/views/not-found/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+export const NotFound: FC = () => {
+  return (
+    <section>404</section>
+  )
+}
