55f7923048a563a58998d79e9b6d2d370da21d19
zhangxu <2590000714@qq.com>
2020-05-06 19:05:05 +0800

    添加sub-menu组件

--- src/components/sub-menu/index.tsx
+++ src/components/sub-menu/index.tsx
@@ -3,6 +3,27 @@ import './index.scss';
 
 export const SubMenu: FC = () => {
   return (
-    <section>次菜单</section>
+    <section className="sub-menu-container">
+      <a href="/detail" className="sub-menu-item">
+        <span>17</span>
+        <span>归档</span>
+      </a>
+      <a href="/detail" className="sub-menu-item">
+        <span>17</span>
+        <span>分类</span>
+      </a>
+      <a href="/detail" className="sub-menu-item">
+        <span>17</span>
+        <span>标签</span>
+      </a>
+      <a href="/detail" className="sub-menu-item">
+        <span>17</span>
+        <span>标签</span>
+      </a>
+      <a href="/detail" className="sub-menu-item">
+        <span>17</span>
+        <span>标签</span>
+      </a>
+    </section>
   )
 }
