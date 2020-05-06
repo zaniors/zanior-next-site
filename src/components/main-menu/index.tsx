0aff84ab604475930986da5d53d22d7276a41b15
zhangxu <2590000714@qq.com>
2020-05-06 11:50:28 +0800

    menu 组件

---
+++ src/components/main-menu/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './index.scss';
+
+export const MainMenu: FC = () => {
+  return (
+    <section>主菜单</section>
+  )
+}
---
+++ src/components/sub-menu/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './index.scss';
+
+export const SubMenu: FC = () => {
+  return (
+    <section>次菜单</section>
+  )
+}
