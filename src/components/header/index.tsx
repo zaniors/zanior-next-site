8c7ddc4f8a4ee2f7f537ed509b9c1f1c913c8bbf
zhangxu <2590000714@qq.com>
2020-05-06 18:48:27 +0800

    header组件

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,14 @@ import './index.scss';
 
 export const Header: FC = () => {
   return (
-    <header>页头</header>
+    <header className="header-container">
+      <section className="avatar">
+        <img src="https://cdn.compelcode.com/image/common/avatar.jpg" alt="avatar" />
+      </section>
+      <h2 className="name">
+        <a href="/">张旭</a>
+      </h2>
+      <p>Yesterday's the past, tomorrow's the future, but today is a GIFT. That's why it's called the present.</p>
+    </header>
   )
 }
