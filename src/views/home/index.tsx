e6474b4f9d6a1434571d96afafdad005ded6ea66
zhangxu <zansderjs@gmail.com>
2020-06-10 15:38:51 +0800

    add site favicon

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,25 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { ArticleLine } from 'zanext';
 import { ArticleOutputData } from '../../model/article';
-import { IArticleLineItemProps } from 'zanext/dist/components/article-line/Item';
 import getArticlesAxios from '../../service/article';
 import './index.scss';
 
-interface ArticleList {
-  id: number;
-  title: string;
-  createTime?: string;
-  cover?: string;
-  intro?: string;
-}
 
 const Home = () => {
-  const [articleListData, setArticleListData] = useState<IArticleLineItemProps[]>([]);
+  const [articleListData, setArticleListData] = useState<ArticleOutputData[]>([]);
 
   const loadArticleList = async () => {
     const { data } = await getArticlesAxios();
-    console.log(data)
-    // setArticleListData(data );
+    setArticleListData(data);
   }
 
   useEffect(() => {
@@ -27,9 +18,7 @@ const Home = () => {
   }, []);
 
   return (
-    <section>
-      <ArticleLine list={articleListData} />
-    </section >
+    <ArticleLine list={articleListData as []} />
   )
 }
 
